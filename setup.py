854ac32334a4e2ee68431298991ae0281dabd1d7
Francesco Riosa <vivo75@gmail.com>
2017-12-17 12:46:12 +0100

    version 0.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='smalltools',
-    version='0.1.2',
+    version='0.1.3',
     description='Small utilities for sysadmins',
     long_description=long_description,
     url='https://github.com/vivo75/smalltools',
